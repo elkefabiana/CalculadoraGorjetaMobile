2e5f0b64f1585e5c8c4696e0dedb5e51d60fa3ea
ElkeFabiana <elkefabiana@ifspcargua.net>
2022-01-12 17:09:06 -0300

    Projetinho pronto basico para aprendizagem

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 import { View, Text, Button } from 'react-native';
 
@@ -45,24 +45,32 @@ const ResultItem = styled.Text`
   margin-top:10px;
   margin-bottom:30px;
 `;
-
-
+const PctArea =styled.View`
+  flex-direction:row;
+  margin:20px;
+`;
+const PctItem =styled.Button`
+`;
 
 export default () => {
 
   const [bill, setBill] = useState('');
   const [tip, setTip] = useState(0);
+  const [pct, setPct] = useState(10);
 
   const calc = () => {
     let nBill = parseFloat(bill);
 
 
     if (nBill) {
-      setTip((10/100) * nBill);
-    } else {
-      alert("Digite o valor da conta");
-    }
+      setTip((pct/100) * nBill);
+    } 
   }
+
+
+  useEffect(()=>{
+   calc(); 
+  },[pct] );
 
   return (
     <Page>
@@ -74,14 +82,22 @@ export default () => {
         value={bill}
         onChangeText={n => setBill(n)}
       />
-      <CalcButton title="Calcular" onPress={calc} />
+
+      <PctArea>
+        <PctItem title="5%"  onPress ={()=>setPct (5)} />
+        <PctItem title="10%" onPress ={()=>setPct(10)} />
+        <PctItem title="15%" onPress ={()=>setPct(15)} />
+        <PctItem title="20%" onPress ={()=>setPct(20)} />
+      </PctArea>
+
+      <CalcButton title={`CALCULAR ${pct}%` } onPress={calc} />
       {tip>0 &&
         <ResultArea>
           <ResultItemTitle>Valor da Conta</ResultItemTitle>
           <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
 
           <ResultItemTitle>Valor da Gorjeta</ResultItemTitle>
-          <ResultItem>R$ {tip.toFixed(2)} (%10)</ResultItem>
+          <ResultItem>R$ {tip.toFixed(2)} ({pct}%)</ResultItem>
 
           <ResultItemTitle>Valor da Total</ResultItemTitle>
           <ResultItem>R$ {(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
